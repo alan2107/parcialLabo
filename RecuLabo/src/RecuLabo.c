94aeebc82c94aaa649b886656230722690fb6091
Alan Abalde <86857138+alan2107@users.noreply.github.com>
2022-07-27 19:07:18 -0300

    librerias

--- RecuLabo/src/RecuLabo.c
+++ RecuLabo/src/RecuLabo.c
@@ -2,9 +2,37 @@
 #include <stdlib.h>
 #include <ctype.h>
 
-
+#include "Utn.h"
+#include "LinkedList.h"
+#include "Controller.h"
+#include "Parser.h"
+#include "Venta.h"
 
 int main(void) {
-	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
-	return EXIT_SUCCESS;
+setbuf(stdout, NULL);
+
+	int auxReturn = -1;
+	linkedList* ventas= NULL;
+	int mainMenuOption;
+	int flagLoadedFile = FALSE;
+	int flagSaveFile = TRUE;
+
+	if((ventas = ll_New())!= NULL){
+		do{
+			MAINMENU:
+			mainMenu();
+		}while()
+	}
+
+			return 0;
+}
+
+
+void mainMenu(void){
+	printf("\n---------------------Menu Principal--------------------"
+			"\n\n1. cargar archivo"
+			"\n2. Imprimir ventas"
+			"\n3. Generar archivo de montos"
+			"\n4. Informes"
+			"\n5. Salir del Programa");
 }
---
+++ RecuLabo/src/Utn.h
@@ -0,0 +1,42 @@
+#include <stdio.h>
+//#include <stdio_ext.h>  /* for Linux */
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+
+#ifndef UTN_H_
+#define UTN_H_
+#define FALSE 0
+#define TRUE 1
+
+
+void utn_anyKey(char* message);
+void utn_err(char* errorMessage, char* anyKeyMessage);
+
+int utn_getInt(int* entry, char* message, char* errorMessage, int minimum, int maximum, int attempts);
+int utn_isInt(char* string, int lenght);
+
+int utn_getFloat(float* entry, char* message, char* errorMessage, float minimum, float maximum, int attempts);
+int utn_isFloat(char* string, int lenght);
+
+int utn_getIden(char* entry, int lenght, char* message, char* errorMessage, int attempts);
+int utn_isIden(char* string, int lenght);
+
+int utn_getName(char* entry, int lenght, char* message, char* errorMessage, int attempts);
+int utn_isName(char* string, int lenght);
+
+int utn_getCode(char* entry, int lenght, char* message, char* errorMessage, int attempts);
+int utn_isCode(char* string, int lenght);
+
+int utn_getText(char* entry, int lenght, char* message, char* errorMessage, int attempts);
+int utn_isText(char* string, int lenght);
+
+int utn_calcAdd(float* sum, float* addend1, float* addend2);
+int utn_calcSub(float* difference, float* minuend, float* subtrahend);
+int utn_calcMul(float* product, float* multiplicand, float* multiplier);
+int utn_calcDiv(float* quotient, int* remainder, float* dividend, float* divisor);
+int utn_calcPer(float* part, float* percentage, float* whole);
+
+
+#endif /* UTN_H_ */s
